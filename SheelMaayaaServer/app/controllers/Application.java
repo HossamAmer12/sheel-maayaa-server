27a9b022fc2799c8c399b1cbcdb474f4bd82f38c
magued.george1990 <magued.george1990@gmail.com>
2011-11-13 20:04:56 +0000

    added method getOffersByFlightNumber

--- SheelMaayaaServer/app/controllers/Application.java
+++ SheelMaayaaServer/app/controllers/Application.java
@@ -77,7 +77,7 @@ public class Application extends Controller {
 			User hashas = User.all(User.class).get();
 			Flight flight = Flight.all(Flight.class).get();
 			
-			Offer offer = new Offer(hashas,flight,(float)1.2,(float)5.0,"Excess weight","NEW");
+			Offer offer = new Offer(hashas,flight,(float)10.0,(float)5.0,"Extra weight","NEW");
 			offer.insert();
 			return "Success";
 			}catch(Exception e){
@@ -85,8 +85,28 @@ public class Application extends Controller {
 			}
 	}
 	
+
+	public static String getOffersByFlightNumber(String flightNumber){
+		
+		try{
+			Flight flight = Flight.all(Flight.class).search(flightNumber, "flightNumber").fetch().get(0);
+			
+			String offerString = "";
+			List<Offer>offers =  flight.offers.fetch();
+			for(int i = 0; i<flight.offers.count();i++)
+			{
+				offerString +=  offers.get(i).toString();
+			}
+			return offerString;
+		}catch(Exception e){
+			return e.toString();
+		}
+	}
+	
+
 	
 	
+
     public static void index() {
         
     	User bob = new User((long)(Math.random()*10000),"balalaika", 
