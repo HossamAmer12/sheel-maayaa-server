40a09773454c3c320a93e3f9c691bf14630b183f
magued.george1990@gmail.com <magued.george1990@gmail.com@71bca46a-e105-69ae-043e-bd855cd3648a>
2011-11-13 20:33:32 +0000

    removed methods from application and added them in Offers.java

--- trunk/SheelMaayaaServer/app/controllers/Application.java
+++ trunk/SheelMaayaaServer/app/controllers/Application.java
@@ -53,61 +53,8 @@ public class Application extends Controller {
 		return e.toString();
 		}
 	}
-	
-	
-	public static String getFlightOffers(long id){
-		try{
-			Flight flight = Flight.getByKey(Flight.class, id);
-			
-			String offerString = "";
-			List<Offer>offers =  flight.offers.fetch();
-			for(int i = 0; i<flight.offers.count();i++)
-			{
-				offerString +=  offers.get(i).toString();
-			}
-			return offerString;
-		}catch(Exception e){
-			return e.toString();
-		}
-	}
-	
-	
-	public static String insertOffer(){
-		try{
-			User hashas = User.all(User.class).get();
-			Flight flight = Flight.all(Flight.class).get();
-			
-			Offer offer = new Offer(hashas,flight,(float)10.0,(float)5.0,"Extra weight","NEW");
-			offer.insert();
-			return "Success";
-			}catch(Exception e){
-			return e.toString();
-			}
-	}
-	
-
-	public static String getOffersByFlightNumber(String flightNumber){
 		
-		try{
-			Flight flight = Flight.all(Flight.class).search(flightNumber, "flightNumber").fetch().get(0);
-			
-			String offerString = "";
-			List<Offer>offers =  flight.offers.fetch();
-			for(int i = 0; i<flight.offers.count();i++)
-			{
-				offerString +=  offers.get(i).toString();
-			}
-			return offerString;
-		}catch(Exception e){
-			return e.toString();
-		}
-	}
-	
-
-	
-	
-
-    public static void index() {
+	public static void index() {
         
     	User bob = new User((long)(Math.random()*10000),"balalaika", 
     			"aa", "", "", 
