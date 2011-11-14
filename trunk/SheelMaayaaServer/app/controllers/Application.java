cc6685e1298b84f6f82aee22933de39726e0a6d0
magued.george1990@gmail.com <magued.george1990@gmail.com@71bca46a-e105-69ae-043e-bd855cd3648a>
2011-11-14 14:47:17 +0000

    trying search() instead of filter()

--- trunk/SheelMaayaaServer/app/controllers/Application.java
+++ trunk/SheelMaayaaServer/app/controllers/Application.java
@@ -46,7 +46,7 @@ public class Application extends Controller {
 	public static String getFlightByAirports(String sourceAirport, String destAirport){
 		
 		try{
-			Flight flight = Flight.all(Flight.class).filter("source", sourceAirport).filter("destination", destAirport).get();
+			Flight flight = Flight.all(Flight.class).search(sourceAirport, "source").search(destAirport, "destination").fetch().get(0);
 			
 	 		return flight.toString();
 		}catch(Exception e){
