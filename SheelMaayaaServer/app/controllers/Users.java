2f83348651a25290b6dd2fa6d6e66abfd70adadd
nada.adly <nada.adly@71bca46a-e105-69ae-043e-bd855cd3648a>
2011-11-15 22:03:29 +0000

    Added insertUser in the Users controller and updated the routes

--- SheelMaayaaServer/app/controllers/Users.java
+++ SheelMaayaaServer/app/controllers/Users.java
@@ -7,10 +7,25 @@ import models.*;
 
 public class Users extends Controller {
 
-	public static String insertUser(String username, String gender, String nationality){
+	/*public static String insertUser(String username, String gender, String nationality){
 		try{
 			
 		User user = new User(username, "ff", "mm", "ll", "", "A12345", "ff.ll@gmail.com", "123", "mm", gender, nationality);
+		user.insert();
+		return "Success";
+		}catch(Exception e){
+		return e.toString();
+		}
+	}
+	
+
+	*/
+	
+	
+	public static String insertUser(String firstName, String middleName, String lastName, String gender, String email, String mobileNumber, String passportNumber, String nationality){
+		try{
+			
+		User user = new User("", firstName, middleName, lastName, "", passportNumber, email, mobileNumber, "", gender, nationality);
 		user.insert();
 		return "Success";
 		}catch(Exception e){
