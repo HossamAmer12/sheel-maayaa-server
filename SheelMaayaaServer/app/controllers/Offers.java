25b58d3210ad7b14464c8dec8745f1856f2041c7
magued.george1990 <magued.george1990@gmail.com>
2011-11-15 22:28:48 +0000

    added testing method

--- SheelMaayaaServer/app/controllers/Offers.java
+++ SheelMaayaaServer/app/controllers/Offers.java
@@ -108,7 +108,7 @@ public class Offers extends Controller {
 			
 			String offerString = "";
 			
-			List<Offer>offers = filterPreferences(flights, userStatus, kgs, price, gender, nationality);
+			List<Offer>offers = offerFilterPreferences(flights, userStatus, kgs, price, gender, nationality);
 			
 			for(int i = 0; i<offers.size();i++)
 			{
@@ -131,7 +131,7 @@ public class Offers extends Controller {
 		
 			String offerString = "";
 			
-			List<Offer>offers = filterPreferences(flights, userStatus, kgs, price, gender, nationality);
+			List<Offer>offers = offerFilterPreferences(flights, userStatus, kgs, price, gender, nationality);
 			
 			for(int i = 0; i<offers.size();i++)
 			{
@@ -145,14 +145,13 @@ public class Offers extends Controller {
 		
 		}
 	
-	public static List<Offer> filterPreferences(List<Flight> flights, int userStatus, int kgs, int price, 
+	public static List<Offer> offerFilterPreferences(List<Flight> flights, int userStatus, int kgs, int price, 
 											String gender, String nationality){
 		try{
 			
 			List<Offer> offersList = new ArrayList<Offer>();
 			
 			Query<Offer> offers;
-
 			
 			for(int i = 0; i<flights.size();i++)
 			{
@@ -160,32 +159,113 @@ public class Offers extends Controller {
 				
 				if(kgs != 0){ 
 						if(userStatus == 0)
-							offers.filter("noOfKilograms!=", kgs);
-				
+							offers.filter("noOfKilograms", kgs);
+	
 						else 
-							offers.filter("noOfKilograms!=", kgs);
+							offers.filter("noOfKilograms", kgs);
 				}
-				
+			
 				if(price != 0){
 						if(userStatus == 0)
-							offers.filter("pricePerKilogram <= ", price);
+							offers.filter("pricePerKilogram", price);
 				
 						else
-							offers.filter("pricePerKilogram >= ", price);
+							offers.filter("pricePerKilogram", price);
 				}
 				
 				//if(kgs != 0 && userStatus == 0)
 					//offers.order("-noOfKilograms");
 				offersList = offers.fetch();
-				
 			}
 			
-			
-			
-			return offersList;
+			if(gender.equals("both") && nationality.equals("none"))
+				return offersList;
+				
+			return userFilterPreferences(offersList, gender, nationality);
 			
 		}catch(Exception e){
 				return null;
 		}
 	}
+	
+	public static List<Offer> userFilterPreferences(List<Offer> offers, String gender, String nationality){
+		try{
+		
+			User user;
+			List<Offer> userFilteredOffers = new ArrayList<Offer>();
+		
+			for(int i = 0 ; i < offers.size() ; i++){
+		
+				user = offers.get(i).user;
+		
+				if(!gender.equals("both") && !nationality.equals("none")){
+					if(gender.equals(user.gender) && nationality.equals(user.nationality))
+						userFilteredOffers.add(offers.get(i));
+				}
+		
+				else if(!gender.equals("both") && nationality.equals("none")){
+					if(gender.equals(user.gender))
+						userFilteredOffers.add(offers.get(i));
+
+				}
+		
+				else if(gender.equals("both") && !nationality.equals("none")){
+					if(nationality.equals(user.nationality))
+						userFilteredOffers.add(offers.get(i));
+				}
+			}
+		
+			return userFilteredOffers;
+		
+		}catch(Exception e){
+			return null;
+		}
+	}
+	
+	public static String testing(String gender, String nationality){
+		try{
+		
+			User user;
+			List<Offer> offers = Offer.all(Offer.class).fetch();
+			
+			List<Offer> userFilteredOffers = new ArrayList<Offer>();
+			
+			String offerString = "";
+			
+			for(int i = 0 ; i < offers.size() ; i++){
+		
+				user = offers.get(i).user;
+		
+				if(!gender.equals("both") && !nationality.equals("none")){
+					if(gender.equals(user.gender) && nationality.equals(user.nationality))
+						userFilteredOffers.add(offers.get(i));
+				}
+		
+				else if(!gender.equals("both") && nationality.equals("none")){
+					if(gender.equals(user.gender))
+						userFilteredOffers.add(offers.get(i));
+
+				}
+		
+				else if(gender.equals("both") && !nationality.equals("none")){
+					if(nationality.equals(user.nationality))
+						userFilteredOffers.add(offers.get(i));
+				}
+				
+				offerString += gender + " , " + user.gender + " ... ";
+			}
+			
+			
+			for(int i = 0; i<userFilteredOffers.size();i++)
+			{
+				offerString +=  userFilteredOffers.get(i).toString();
+			}
+			return offerString;
+			
+		}catch(Exception e){
+			return e.toString();
+		}
+	}
+	
+	
 }
