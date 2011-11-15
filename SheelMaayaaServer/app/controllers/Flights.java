3fa1843ba995312653e18cc9fe46e5fa0ccd4b10
magued.george1990 <magued.george1990@gmail.com>
2011-11-15 19:56:59 +0000

    added 2 methods to get flights by flight number and airports

--- SheelMaayaaServer/app/controllers/Flights.java
+++ SheelMaayaaServer/app/controllers/Flights.java
@@ -3,6 +3,7 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import java.util.*;
+
 import models.*;
 
 public class Flights extends Controller {
@@ -18,5 +19,31 @@ public class Flights extends Controller {
 		}
 	}
 	
-
+	public static List<Flight> getFlightsByFlightNumber(String flightNumber, String date){
+		
+		try{
+			
+			List<Flight>flights = Flight.all(Flight.class).filter("flightNumber", flightNumber)
+						.filter("departureDate", date).fetch();
+				
+			return flights;
+			
+		}catch(Exception e){
+			return null;
+		}
+	}
+	
+	public static List<Flight> getFlightsByAirports(String source, String destination, String date){
+		
+		try{
+			
+			List<Flight>flights = Flight.all(Flight.class).filter("source", source).
+					filter("destination", destination).filter("departureDate", date).fetch();
+				
+			return flights;
+			
+		}catch(Exception e){
+			return null;
+		}
+	}
 }
