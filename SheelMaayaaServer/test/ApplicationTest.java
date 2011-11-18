58b24253917d6fa378b8ab2ffa90b4524da13138
hossam.amer12 <hossam.amer12@gmail.com>
2011-11-18 18:48:43 +0000

    Refactored the code.

--- SheelMaayaaServer/test/ApplicationTest.java
+++ SheelMaayaaServer/test/ApplicationTest.java
@@ -15,22 +15,13 @@ import play.mvc.Http.Response;
 import play.test.FunctionalTest;
 
 public class ApplicationTest extends FunctionalTest {
-
-//    @Test
-//    public void testThatIndexPageWorks() {
-//        Response response = GET("/");
-//        assertIsOk(response);
-//        assertContentType("text/html", response);
-//        assertCharset(play.Play.defaultWebEncoding, response);
-//    }
     
 	/**
 	 * @author Hossam_Amer
 	 * Tests whether the insertConfimation controller working
 	 * 
 	 */
-	static int count = 0;
-	static Timer x;
+
 	static String response;
 	
     @Test
