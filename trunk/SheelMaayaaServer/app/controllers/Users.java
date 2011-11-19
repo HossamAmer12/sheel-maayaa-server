4186f50d74f69a7cdabe943863bd53983c06cb45
nada.adly <nada.adly@71bca46a-e105-69ae-043e-bd855cd3648a>
2011-11-19 13:40:54 +0000

    Updated insertUser method to inser User facebook ID and aupdated the routes

--- trunk/SheelMaayaaServer/app/controllers/Users.java
+++ trunk/SheelMaayaaServer/app/controllers/Users.java
@@ -7,25 +7,24 @@ import models.*;
 
 public class Users extends Controller {
 
-	/*public static String insertUser(String username, String gender, String nationality){
+
+	
+	
+	/* public static String insertUser(String firstName, String middleName, String lastName, String gender, String email, String mobileNumber, String passportNumber, String nationality, String passportPhoto){
 		try{
 			
-		User user = new User(username, "ff", "mm", "ll", "", "A12345", "ff.ll@gmail.com", "123", "mm", gender, nationality);
+		User user = new User("", firstName, middleName, lastName, passportPhoto, passportNumber, email, mobileNumber, "", gender, nationality);
 		user.insert();
 		return "Success";
 		}catch(Exception e){
 		return e.toString();
 		}
 	}
-	
-
 	*/
-	
-	
-	public static String insertUser(String firstName, String middleName, String lastName, String gender, String email, String mobileNumber, String passportNumber, String nationality, String passportPhoto){
+	 public static String insertUser(String facebookID, String firstName, String middleName, String lastName, String gender, String email, String mobileNumber, String passportNumber, String nationality, String passportPhoto){
 		try{
 			
-		User user = new User("", firstName, middleName, lastName, passportPhoto, passportNumber, email, mobileNumber, "", gender, nationality);
+		User user = new User("", firstName, middleName, lastName, passportPhoto, passportNumber, email, mobileNumber, facebookID, gender, nationality);
 		user.insert();
 		return "Success";
 		}catch(Exception e){
