858850def4aa4d41fe3f4364f01787dd09c682bf
hossam.amer12@gmail.com <hossam.amer12@gmail.com@71bca46a-e105-69ae-043e-bd855cd3648a>
2011-12-13 21:41:07 +0000

    Cleaned up the application file

--- trunk/SheelMaayaaServer/app/controllers/Application.java
+++ trunk/SheelMaayaaServer/app/controllers/Application.java
@@ -18,70 +18,9 @@ public class Application extends Controller {
 		return "Test successful";
 		
 	}
-	/*
-	public static String insertFlight(CFlight flight){
-		
-		try{
-			Flight f = new Flight(flight);
-	 	    f.insert();
-	 	    return "Success";
-		}catch(Exception e){
-			return e.toString();
-		}
-		
-	}
 	
-	public static String getFlightByFlightNumber(String flightNumber){
 		
-		try{
-			Flight flight = Flight.all(Flight.class).search(flightNumber, "flightNumber").fetch().get(0);
-			
-	 		return flight.toString();
-		}catch(Exception e){
-			return e.toString();
-		}
-		
-	}
-	
-	public static String getFlightByAirports(String source, String destination){
-		
-		try{
-			Flight flight = Flight.all(Flight.class).search(source, "source").search(destination, "destination").get();
-						
-	 		return flight.toString();
-		}catch(Exception e){
-			return e.toString();
-		}
-		
-	}
-
-	public static String insertFlight(){
-		try{
-			
-		Flight f = new Flight("LH123", "Frankfurt", "Cairo", "11-15-2011");
-		f.insert();
-		return "Success";
-		}catch(Exception e){
-		return e.toString();
-		}
-	}*/
-		
-	public static void index() {
-        
-//    	User bob = new User("balalaika", 
-//    			"aa", "", "", 
-//    			"", "", "", "", "","","");
-//    	
-//    	bob.insert();
-//    	
-//    	User hashas = User.all(User.class).get();
-//    	
-//    	renderJSON (hashas);
-
-		//For testing the bootstrap
-//		User x = User.all(User.class).get();
-//		renderJSON(x);
-		
+	public static void index() {		
 		render();
     }
     
