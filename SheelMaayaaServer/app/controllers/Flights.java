230d4cf3ac2333cfc19a5eda1c8697a65ae0102d
magued.george1990 <magued.george1990@gmail.com>
2011-12-16 18:50:07 +0000

    deleted unused methods

--- SheelMaayaaServer/app/controllers/Flights.java
+++ SheelMaayaaServer/app/controllers/Flights.java
@@ -52,32 +52,4 @@ public class Flights extends Controller {
 			return e.toString();
 		}
 	}
-	
-	public static List<Flight> getFlightsByFlightNumber(String flightNumber, String date){
-		
-		try{
-			
-			List<Flight>flights = Flight.all(Flight.class).filter("flightNumber", flightNumber)
-						.filter("departureDate", date).fetch();
-				
-			return flights;
-			
-		}catch(Exception e){
-			return null;
-		}
-	}
-	
-	public static List<Flight> getFlightsByAirports(String source, String destination, String date){
-		
-		try{
-			
-			List<Flight>flights = Flight.all(Flight.class).filter("source", source).
-					filter("destination", destination).filter("departureDate", date).fetch();
-				
-			return flights;
-			
-		}catch(Exception e){
-			return null;
-		}
-	}
 }
